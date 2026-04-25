cd0b63167506e0e6409be1c5353cdf53f786d989
Farhan Ansari <farhan.knp121@gmail.com>
2026-04-25 16:38:31 +0530

    feat(publish): implement component API and npm publish workflow

--- llmighty-ui-lib/src/index.js
+++ llmighty-ui-lib/src/index.js
@@ -1,3 +1,6 @@
 export {Button} from "./components/Button/Button.jsx";
 export {Card} from "./components/Card/Card.jsx";
 export {ProfileCard} from "./components/Profilecard/ProfileCard.jsx";
+export { NotesApp } from "./components/NotesApp/NotesApp.jsx";
+
+export { Sidebar } from "./components/Sidebar/Sidebar.jsx";
--- server/routes/component.route.js
+++ server/routes/component.route.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import isAuth from '../middlewares/isAuth.js'
-import { generateComponent } from '../controllers/aicomponent.controller.js'
-import { publishComponent, saveComponent } from '../controllers/component.controller.js'
+import express from "express";
+import isAuth from "../middlewares/isAuth.js";
+import { generateComponent } from "../controllers/aicomponent.controller.js";
+import {
+  saveComponent,
+  publishComponent,
+  getMyComponents,
+  deleteComponent,
+} from "../controllers/component.controller.js";
 
-const componentRouter = express.Router()
+const componentRouter = express.Router();
 
-componentRouter.post("/generate",isAuth,generateComponent)
+componentRouter.post("/generate", isAuth, generateComponent);
+componentRouter.post("/save", isAuth, saveComponent);
+componentRouter.post("/publish", isAuth, publishComponent);
+componentRouter.get("/my", isAuth, getMyComponents);
+componentRouter.delete("/:id", isAuth, deleteComponent);
 
-componentRouter.post("/save",isAuth,saveComponent)
-
-componentRouter.post("/publish",isAuth,publishComponent)
-
-export default componentRouter
+export default componentRouter;
